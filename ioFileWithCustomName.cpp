8d71922a8b41c549ae239eaf932e705dff769c92
Deva658 <devaaditya271@gmail.com>
2025-06-09 15:49:08 +0700

    menambahkan fungsi main

---
+++ ioFileWithCustomName.cpp
@@ -0,0 +1,56 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string  baris;
+    string namefile;
+
+    cout << "masukan name file : ";
+    cin >> namefile;
+    cin.ignore();
+
+    //membuat objek ouput file stream
+    ofstream oufile;
+    //membuka file untuk menulis
+    oufile.open("contohfile.txt");
+
+    cout << ">= menulis file, \'q\' untuk keluar\n";
+
+    //unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+        //mendapatkan stiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berenti jika anda memasukan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukan nilai dari 'baris ke dalam file
+        oufile << baris << endl;
+    }
+    //selesai dalam menulis sekarang tutup filenya
+    oufile.close();
+
+    //membuat objek input file stream
+    ifstream infile;
+    //membuka file yang dituliskan
+    infile.open(namefile + ".txt");
+
+    cout << endl << ">= membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open()) 
+    {
+        //membaca file baris per baris
+        while (getline(infile, baris)) 
+        {
+            //menampilkan setiap baris yang dibaca
+            cout << baris << '\n';
+        }
+        //tutup file tersebut di sini
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilakn ini
+    else cout << "unable to open file";
+    return 0;
+}
